b999980fb9620900602612e472f3297bfa2bef88
Josh <jsmith07@uw.edu>
2018-11-19 10:36:45 -0800

    add histogram density tests

--- sgoop/tests/test_analysis.py
+++ sgoop/tests/test_analysis.py
@@ -13,22 +13,39 @@ def test_gaussian_denisty_estimation():
 
     # bandwidth too small for Gaussians to overlap
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.001)
-    assert(np.all(actual[actual == actual[0]]))
+    assert np.all(actual[actual == actual[0]])
 
     # bandwidth large enough for Gaussians to overlap
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.5)
     expected = np.array([[0.18122683, 0.20282322, 0.20287675, 0.20282322, 0.18122683]])
-    assert(np.allclose(actual, expected))
+    assert np.allclose(actual, expected)
 
     # weighted samples with overlapping Gaussians
     weights = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5])
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.5)
     expected = np.array([0.06764343, 0.13525117, 0.20287675, 0.27039527, 0.29481024])
-    assert(np.allclose(actual, expected))
-    
+    assert np.allclose(actual, expected)
+
 
 def test_histogram_density_estimation():
-    assert True
+    samples = np.array([0, 1, 2, 3, 4, 0, 1, 2, 3, 4])
+    weights = None
+    bins = 3
+
+    # unweighted histogram (probability mass function)
+    actual_hist, actual_edges = histogram_density_estimation(samples, weights, bins)
+    expected_hist = np.array([0.3, 0.15, 0.3])
+    expected_edges = np.array([0.0, 1.33333333, 2.66666667, 4.0])
+    assert np.allclose(actual_hist, expected_hist)
+    assert np.allclose(actual_edges, expected_edges)
+
+    # weighted histogram (probability mass function)
+    weights = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5])
+    actual_hist, actual_edges = histogram_density_estimation(samples, weights, bins)
+    expected_hist = np.array([0.15, 0.15, 0.45])
+    expected_edges = np.array([0.0, 1.33333333, 2.66666667, 4.0])
+    assert np.allclose(actual_hist, expected_hist)
+    assert np.allclose(actual_edges, expected_edges)
 
 
 def test_find_closest_points():
