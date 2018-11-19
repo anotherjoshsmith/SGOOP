3f3f044fc86f18652d8d9d739fe5f667be4116b3
Josh <jsmith07@uw.edu>
2018-11-19 10:27:19 -0800

    fix gaussian kde tests

--- sgoop/tests/test_analysis.py
+++ sgoop/tests/test_analysis.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sgoop.analysis import gaussian_density_estimation
-# from sgoop.analysis import histogram_density_estimation
+from sgoop.analysis import histogram_density_estimation
 # from sgoop.analysis import avg_neighbor_transitions
 # from sgoop.analysis import probability_matrix
 # from sgoop.analysis import sorted_eigenvalues
@@ -13,7 +13,7 @@ def test_gaussian_denisty_estimation():
 
     # bandwidth too small for Gaussians to overlap
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.001)
-    assert(np.allclose(actual, actual))
+    assert(np.all(actual[actual == actual[0]]))
 
     # bandwidth large enough for Gaussians to overlap
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.5)
@@ -25,7 +25,7 @@ def test_gaussian_denisty_estimation():
     actual = gaussian_density_estimation(samples, weights, samples[:5], h=0.5)
     expected = np.array([0.06764343, 0.13525117, 0.20287675, 0.27039527, 0.29481024])
     assert(np.allclose(actual, expected))
-
+    
 
 def test_histogram_density_estimation():
     assert True
