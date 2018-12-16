c277c1d40da20e69ba11de85b0f46c3eb47cab3c
Josh <jsmith07@uw.edu>
2018-12-16 14:06:03 -0800

    add boring trivial visualization tests to build on

---
+++ sgoop/tests/test_visualization.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+from sgoop.visualization import plot_spectral_gap
+from sgoop.visualization import plot_pmf
+
+
+def test_plot_spectral_gap():
+    rc = np.array([0.3, 0.5, 0.8])
+    samples = np.array(
+        [[0, 1, 2],
+         [3, 4, 0],
+         [1, 2, 3]]
+    )
+    sgoop_dict = {
+        'rc_bins': 3,
+        'wells': 2,
+        'd': 1,
+        'diffusivity': 10,
+    }
+
+    # test that plot_spectral_gap returns something
+    ax = plot_spectral_gap(rc, samples, sgoop_dict)
+    assert ax is not None
+
+
+def test_plot_pmf():
+    rc = np.array([0.3, 0.5, 0.8])
+    samples = np.array(
+        [[0, 1, 2],
+         [3, 4, 0],
+         [1, 2, 3]]
+    )
+    sgoop_dict = {
+        'rc_bins': 3,
+        'wells': 2,
+        'd': 1,
+        'diffusivity': 10,
+    }
+
+    # test that plot_pmf returns something
+    ax = plot_pmf(rc, samples, sgoop_dict)
+    assert ax is not None
